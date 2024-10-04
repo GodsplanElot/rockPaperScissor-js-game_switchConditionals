741ea458605bf7b07c6920ef0f1c1f0334ba856b
GodsplanELot <elotgodsplan2@gmail.com>
2024-10-04 17:38:07 +0100

    wrote the logic structure for the switch logic

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 // Conditionals: Switch Statements
+
+//switch (expression or value //basically the condition) {
+
+    //case choice1:
+        // run this code
+       // break;
+
+    //case choice2:
+        // run this different code
+        //break;
+    
+    // add as many cases as needed
+
+    //default:
+        // run this code if no case matches
+        // no need for a break here
+//}
+
+
+
+// Now lets bulid our logic using switch statements
+
+switch ("2") {
+
+    case 1:
+        // run this code
+        break;
+
+    case 2:
+        // run this code
+        break;
+
+    case 3:
+        // run this code
+        break;
+
+}
